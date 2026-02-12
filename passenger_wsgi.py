f750d4f35be0702b96ab37ddb4133800db43a589
felipemiramontesr <felipemiramontesr@gmail.com>
2026-02-11 22:38:39 -0600

    Style: Format Hello World app with Black

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,8 +4,9 @@ import os
 # Minimal WSGI Application for Isolation Testing
 # No imports from 'api', no external libs. just pure python.
 
+
 def application(environ, start_response):
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
+    status = "200 OK"
+    response_headers = [("Content-type", "text/plain")]
     start_response(status, response_headers)
     return [b"Hello World from Passenger! The server is running correctly."]
