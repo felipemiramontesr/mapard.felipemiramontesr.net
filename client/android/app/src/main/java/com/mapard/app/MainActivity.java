0c6016d574cd300bc133d14f3af32373dcd3a660
felipemiramontesr <felipemiramontesr@gmail.com>
2026-02-13 15:05:51 -0600

    Fix(Android): Implement Theme Switch and Dark Base to resolve Keyboard Occlusion and White Flash

--- client/android/app/src/main/java/com/mapard/app/MainActivity.java
+++ client/android/app/src/main/java/com/mapard/app/MainActivity.java
@@ -1,5 +1,13 @@
 package com.mapard.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Switch to AppTheme.NoActionBar to enable status bar and keyboard resize
+        setTheme(R.style.AppTheme_NoActionBar);
+    }
+}
