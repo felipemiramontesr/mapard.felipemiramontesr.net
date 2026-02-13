4a1aeb004e712023a54a095eee3504fd0e253e67
felipemiramontesr <felipemiramontesr@gmail.com>
2026-02-13 02:14:48 -0600

    Feat(Splash): Configure seamless transition to 5s Matrix Loader

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -11,6 +11,14 @@ const config: CapacitorConfig = {
         StatusBar: {
             overlaysWebView: true,
             style: 'DARK'
+        },
+        SplashScreen: {
+            backgroundColor: "#000000",
+            launchShowDuration: 0,
+            showSpinner: false,
+            androidScaleType: "CENTER_CROP",
+            splashFullScreen: true,
+            splashImmersive: true
         }
     }
 };
