ed551ece08df51706ecd077d0292b67e8e19c634
felipemiramontesr <felipemiramontesr@gmail.com>
2026-03-05 00:18:26 -0600

    chore(debug): add test connection script and whitelist it in htaccess

---
+++ api/test.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/plain; charset=utf-8');
+echo "TEST MAPARD CONNECTION SUCCESSFUL\n";
+echo "Timestamp: " . date("Y-m-d H:i:s") . "\n";
+echo "If you can see this, PHP is executing correctly.\n";
+?>
